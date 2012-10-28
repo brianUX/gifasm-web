dce46ff962df481ceeed4d13e869f62b761bda3d
brian andrich <brianandrich@gmail.com>
2012-10-28 00:03:53 -0400

    bunch of shit

--- js/gifasm.js
+++ js/gifasm.js
@@ -14,14 +14,15 @@ $(function(){
 		//all gifs view
 		AllGifsView = Parse.View.extend({
 			el: $('.content'),
-			template: _.template($('#gifs-template').html()),
+			template: _.template($('#gallery-gif-template').html()),
 			initialize: function(){
-				_.bindAll(this);
+				_.bindAll(this, "sizer");
 				var self = this;
-				this.gifs = new Gifs();
-				this.gifs.fetch({
-					success: function(collection) {
-						self.render(collection);
+				this.query = new Gifs();
+				this.query.fetch({
+					success: function(gifs) {
+						$(".content").empty();
+						self.render(gifs);
 					},
 					error: function(collection, error) {
 					    // The collection could not be retrieved.
@@ -31,27 +32,219 @@ $(function(){
 			},
 			render: function(gifs){
 				var self = this;
-				gifs.each(function(gif) {
-					$(self.el).append(self.template({ 
-						src: gif.attributes.src,
-						id: gif.id
-					}));
+				gifs.each(function(thisgif) {
+					var data = {
+						src: thisgif.attributes.src,
+						id: thisgif.id,
+						tags: thisgif.attributes.tags 
+					};
+					$(self.el).prepend(self.template(data));
+				});
+				new PlayGallery();
+				this.sizer();
+			},
+			sizer: function() {
+				var width = window.innerWidth;
+				var height = window.innerHeight - 64;
+				var gifs = $(".content .gif-container img");
+				gifs.css({
+					"width": width,
+					"height": height
+				});
+				$(window).resize(function(){
+					var width = window.innerWidth;
+					var height = window.innerHeight -64;
+					var gifs = $(".content .gif-container img");
+					gifs.css({
+						"width": width,
+						"height": height
+					});
 				});
 			}
 		});
 		
+		//user gifs view
+		UserGifsView = Parse.View.extend({
+			el: $('.content'),
+			template: _.template($('#gallery-gif-template').html()),
+			initialize: function(){
+				_.bindAll(this, "sizer");
+				var self = this;
+				var username = this.options.username;
+				//grab all gifs by user
+				var gifs = new Parse.Query("Gif");
+				gifs.matches("username", username);
+				gifs.find({
+					success: function(gifs) {
+						$(".content").empty();
+						self.render(gifs);
+					}, 
+					error: function() {
+						alert('couldnt find user gifs');
+					}
+				});
+			},
+			render: function(gifs){
+				var self = this;
+				if (gifs.length > 0) {
+					gifs.forEach(function(thisgif) {
+						var data = {
+							src: thisgif.attributes.src,
+							id: thisgif.id,
+							tags: thisgif.attributes.tags 
+						};
+						$(self.el).prepend(self.template(data));
+					});	
+					new PlayGallery();
+					this.sizer();
+				} else {
+					alert("this user has no gifs");
+				}
+			},
+			sizer: function() {
+				var width = window.innerWidth;
+				var height = window.innerHeight - 64;
+				var gifs = $(".content .gif-container img");
+				gifs.css({
+					"width": width,
+					"height": height
+				});
+				$(window).resize(function(){
+					var width = window.innerWidth;
+					var height = window.innerHeight -64;
+					var gifs = $(".content .gif-container img");
+					gifs.css({
+						"width": width,
+						"height": height
+					});
+				});
+			}
+		});
+		
+		//tagged gifs view
+		TagGifsView = Parse.View.extend({
+			el: $('.content'),
+			template: _.template($('#gallery-gif-template').html()),
+			initialize: function (){
+				_.bindAll(this, "sizer");
+				var self = this;
+				var tag = this.options.tag;
+				//grab all gifs by user
+				var query = new Parse.Query("Gif");
+				query.equalTo("tags", tag);
+				query.find({
+					success: function(gifs) {
+						$(".content").empty();
+						self.render(gifs);
+					}, 
+					error: function() {
+						alert('couldnt grab any gifs by tag');
+					}
+				});
+			},
+			render: function(gifs){
+				var self = this;
+				if (gifs.length > 0) {
+					gifs.forEach(function(thisgif) {
+						var data = {
+							src: thisgif.attributes.src,
+							id: thisgif.id,
+							tags: thisgif.attributes.tags 
+						};
+						$(self.el).prepend(self.template(data));
+					});	
+					new PlayGallery();
+					this.sizer();
+				} else {
+					alert("couldnt find any gifs with that tag");
+				}
+			},
+			sizer: function() {
+				var width = window.innerWidth;
+				var height = window.innerHeight - 64;
+				var gifs = $(".content .gif-container img");
+				gifs.css({
+					"width": width,
+					"height": height
+				});
+				$(window).resize(function(){
+					var width = window.innerWidth;
+					var height = window.innerHeight -64;
+					var gifs = $(".content .gif-container img");
+					gifs.css({
+						"width": width,
+						"height": height
+					});
+				});
+			}
+		});
+		
+			//play gallery
+			PlayGallery = Parse.View.extend({
+				el: $('.content'),
+				events: {
+		     		"click a.gif": "next"
+				},
+				initialize: function() {
+					_.bindAll(this, "next");
+					var item = $(".content .gallery-gif");
+					var size = item.length;
+					//load first five gifs
+					if (size < 5) {
+						item.each(function() {
+							$(this).removeClass("unloaded").addClass("loaded");
+							var img = $(this).find("img");
+							var src = img.attr("data-src");
+							img.attr("src", src);
+						});
+					} else {
+						item.slice(0,5).each(function() {
+							$(this).removeClass("unloaded").addClass("loaded");
+							var img = $(this).find("img");
+							var src = img.attr("data-src");
+							img.attr("src", src);
+						});
+					}
+					//show first one
+					item.eq(0).removeClass("hide");
+				},
+				next: function() {
+					var current = $(".content .gallery-gif:visible");
+					var next = current.next(".gallery-gif");
+					//hide current
+					current.addClass("hide");
+					//show next
+					if (next.length) {
+						next.removeClass("hide");
+						//load next unloaded gif
+						var ondeck = $(".content .gallery-gif.unloaded").eq(0);
+						if (ondeck.length) {
+							ondeck.removeClass("unloaded").addClass("loaded");
+							var img = ondeck.find("img");
+							var src = img.attr("data-src");
+							img.attr("src", src);
+						}
+					} else {
+						$(".content .gallery-gif").eq(0).removeClass("hide");
+					}
+				}	
+			});
+			
+		
 		//single gif view
 		SingleGifView = Parse.View.extend({
 			el: $('.content'),
-			template: _.template($('#gifs-template').html()),
+			template: _.template($('#single-gif-template').html()),
 			initialize: function(){
-				_.bindAll(this);
+				_.bindAll(this, "sizer");
 				var self = this;
 				var singleGif = Parse.Object.extend("Gif");
 				this.gif = new Parse.Query(singleGif);
 				this.gif.get(this.options.gif, {
 				  	success: function(gif) {
+						$(".content").empty();
 						self.render(gif);
+						var tags = gif.attributes.tags;
 				  	},
 				  	error: function(gif, error) {
 						alert('shit');
@@ -59,58 +252,71 @@ $(function(){
 				});
 			},
 			render: function(thisgif){
-				$(this.el).html(this.template({ 
+				var data = {
 					src: thisgif.attributes.src,
-					id: thisgif.id
-				 }));
+					id: thisgif.id,
+					tags: thisgif.attributes.tags 
+				};
+				$(this.el).html(this.template(data));	
+				this.sizer();
+			},
+			sizer: function() {
+				var width = window.innerWidth;
+				var height = window.innerHeight - 64;
+				var gifs = $(".content .gif-container img");
+				gifs.css({
+					"width": width,
+					"height": height
+				});
+				$(window).resize(function(){
+					var width = window.innerWidth;
+					var height = window.innerHeight -64;
+					var gifs = $(".content .gif-container img");
+					gifs.css({
+						"width": width,
+						"height": height
+					});
+				});
 			}
 		});
 		
-		//user gifs view
-		UserGifsView = Parse.View.extend({
-			el: $('.content'),
-			template: _.template($('#gifs-template').html()),
-			initialize: function(){
-				_.bindAll(this);
-				var self = this;
-				var username = this.options.username;
-				//grab all gifs by user
-				var gifs = new Parse.Query("Gif");
-				gifs.equalTo("username", username);
-				gifs.find({
-					success: function(gifs) {
-						if (gifs.length > 0) {
-							gifs.forEach(self.render);	
-						} else {
-							alert("this user has no gifs");
-						}
-					}, 
-					error: function() {
-						alert('couldnt find user gifs');
-					}
-				});
+		//search view
+	    SearchView = Parse.View.extend({
+			el: $('.search'),
+			template: _.template($('#search-template').html()),
+			events: {
+	     		"submit form.search": "search"
 			},
-			render: function(gif){
-				$(this.el).append(this.template({ 
-					src: gif.attributes.src,
-					id: gif.id
-				}));
-			}
+			initialize: function() {
+			  _.bindAll(this, "search");
+			  this.render();
+			},
+			render: function() {
+			  	$(this.el).html(this.template());
+			},
+			search: function() {
+				var query = $("form.search input").val();
+				if (query) {
+					app.navigate("/tag/"+query+"", {trigger: true});	
+				}
+		    	return false;
+		    }
 		});
 	
 		//logged out view
 		LoggedOutView = Parse.View.extend({
+			el: ".user",
+			template: _.template($('#nonuser-template').html()),
 			events: {
 			  "submit form.login-form": "logIn",
 			  "submit form.signup-form": "signUp"
 			},
-			el: ".user",
 			initialize: function() {
 			    _.bindAll(this, "logIn", "signUp");
 			    this.render();
 			},
 			render: function() {
-			  this.$el.html(_.template($("#login-template").html()));
+			  this.$el.html(this.template());
 			},
 			logIn: function(e) {
 			  var self = this;
@@ -154,7 +360,7 @@ $(function(){
 		//logged in view
 		LoggedInView = Parse.View.extend({
 			el: $('.user'),
-			template: _.template($('#logout-template').html()),
+			template: _.template($('#user-template').html()),
 			events: {
 	     		"click .log-out": "logOut"
 			},
@@ -182,7 +388,7 @@ $(function(){
 				"click #uploadbutton": "uploadGif",
 				"click #urladdbutton": "addUrl",
 			},
-			el: ".user",
+			el: ".loggedin .add",
 			template: _.template($('#upload-template').html()),
 			appid: "lwRB5rPvenfJwKYSeDtnCsXj4WNZa3PuwAyAIN3P",
 			restkey: "LfkvpLyFErkF84FPPoZIOzOvSNH10jQI1meGnLEr",
@@ -214,6 +420,7 @@ $(function(){
 					processData: false,
 					contentType: false,
 					success: function(data) {
+						console.log(data);
 						self.addGif(data.url);
 					},
 					error: function(data) {
@@ -227,14 +434,21 @@ $(function(){
 				var user = Parse.User.current();
 				var username = user.get("username");
 				var userid = user.id;
+				var tags = $("input[name=hidden-tags]").val();
 				var gif = new Gif();
 				gif.set("src", src);
 				gif.set("username", username);
 				gif.set("userid", userid);
+				if (tags) {
+					var tags = tags.split(',');
+					gif.set("tags", tags);
+				} else {
+					alert('notags')
+				};
 				gif.save(null, {
 					success: function(newgif) {
 						self.addToUser(newgif);
-						app.navigate("/g/"+newgif.id+"", {trigger: true});
+						app.navigate("/gif/"+newgif.id+"", {trigger: true});
 						delete self;
 				  	},
 					error: function() {
@@ -264,10 +478,12 @@ $(function(){
 
 		//app view
 		AppView = Parse.View.extend({
-			initialize: function(){
+			el: ".content",
+			initialize: function() {
+				_.bindAll(this);
 		        this.render();
 		    },
-		    render: function(){
+		    render: function() {
 				var currentUser = Parse.User.current();
 				if (currentUser) {
 					new LoggedInView();
@@ -275,18 +491,18 @@ $(function(){
 				} else {
 					new LoggedOutView();
 				}
-		    },
-		    events: {
-
+				new SearchView();
 		    }
 		});
+		
 	
 	//router
 	var AppRouter = Parse.Router.extend({
 		routes: {
 			""    :    "home",
-			"g/:id" : "singleGif",
-			":username" : "userGallery"
+			"gif/:id" : "singleGif",
+			":username" : "userGallery",
+			"tag/:tag" : "tagGallery"
 		},
 		initialize: function() {
 	        new AppView();
@@ -303,8 +519,12 @@ $(function(){
 			new UserGifsView({
 				username: username
 			});
+		},
+		tagGallery: function(tag) {
+			new TagGifsView({
+				tag: tag
+			});
 		}
-		
 	});
 	
 	var app = new AppRouter();
